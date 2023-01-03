7317363766a2207a95420f14dcbab968a836d6ef
Awad Abdelrahman <aawad7526@upei.ca>
2023-01-03 13:23:40 -0400

    added 4 extra particle systems to blow snow from all directions. adjusted wind script to support more particle systems

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -11,14 +11,17 @@ public class Wind : MonoBehaviour
     [SerializeField] private float windIntesity = 2, windDirectionTimeSlotMin, windDirectionTimeSlotMax;
     public Vector2 oldWindDir, currentWindDir, newWindDir;
     private float windDirectionTimeSlot, windDirectionCountDown = 0;
-    private ParticleSystem particleSys;
-    private ParticleSystem.VelocityOverLifetimeModule particleVelOverLifeTime;
+    public ParticleSystem particleSys1, particleSys2, particleSys3, particleSys4, particleSys5;
+    private ParticleSystem.VelocityOverLifetimeModule particleVelOverLifeTime1, particleVelOverLifeTime2, particleVelOverLifeTime3, particleVelOverLifeTime4, particleVelOverLifeTime5;
     private ParticleSystem.MinMaxCurve minMaxX, minMaxY, minMaxZ;
     // Start is called before the first frame update
     void Start()
     {
-        particleSys = GetComponentInChildren<ParticleSystem>();
-        particleVelOverLifeTime = particleSys.velocityOverLifetime;
+        particleVelOverLifeTime1 = particleSys1.velocityOverLifetime;
+        particleVelOverLifeTime2 = particleSys2.velocityOverLifetime;
+        particleVelOverLifeTime3 = particleSys3.velocityOverLifetime;
+        particleVelOverLifeTime4 = particleSys4.velocityOverLifetime;
+        particleVelOverLifeTime5 = particleSys5.velocityOverLifetime;
 
         //particleVelOverLifeTime.y = new ParticleSystem.MinMaxCurve(-1, -3);
 
@@ -42,8 +45,20 @@ public class Wind : MonoBehaviour
         minMaxX = new ParticleSystem.MinMaxCurve(currentWindDir.x, currentWindDir.x * windIntesity);
         minMaxZ = new ParticleSystem.MinMaxCurve(currentWindDir.y, currentWindDir.y * windIntesity); //wind direction is 2d so y is z in this case
 
-        particleVelOverLifeTime.x = minMaxX;
-        particleVelOverLifeTime.z = minMaxZ;
+        particleVelOverLifeTime1.x = minMaxX;
+        particleVelOverLifeTime1.z = minMaxZ;
+
+        particleVelOverLifeTime2.y = new ParticleSystem.MinMaxCurve(-currentWindDir.x, -currentWindDir.x * windIntesity);
+        particleVelOverLifeTime2.z = minMaxZ;
+
+        particleVelOverLifeTime3.y = new ParticleSystem.MinMaxCurve(-currentWindDir.x, -currentWindDir.x * windIntesity);
+        particleVelOverLifeTime3.z = minMaxZ;
+
+        particleVelOverLifeTime4.z = minMaxX;
+        particleVelOverLifeTime4.y = minMaxZ;
+
+        particleVelOverLifeTime5.z = minMaxX;
+        particleVelOverLifeTime5.y = minMaxZ;
 
         windDirectionCountDown -= Time.deltaTime;
 
