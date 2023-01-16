1ec4c24177b3cb648bf9bbf16376d8c6e989140c
Awad Abdelrahman <aawad7526@upei.ca>
2023-01-16 00:59:15 -0500

    Fixed bug where frost was accumulating on the screen when not facing wind

--- Assets/Scripts/WeatherController.cs
+++ Assets/Scripts/WeatherController.cs
@@ -1,19 +1,109 @@
+/*
+Written by: Abdelrahman Awad
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WeatherController : MonoBehaviour
 {
-    
+    //Post processing effects
+    [SerializeField] private Camera playerCam;
+
+    public float accumulationRate = 5.0f;
+    public float meltingRate = 10.0f;
+    public float timeBeforeMelt = 2;
+    private float frostIntensity = 0;
+    private float distort = 0;
+    private bool firstMelt = false;
+
+    //Wind
+    private Wind wind;
+    private Vector2 windDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        wind = GetComponentInChildren<Wind>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(PPEffects());
+    }
+
+    IEnumerator PPEffects()
+    {
+        Vector2 windDir = wind.currentWindDir;
+        float dotP = Vector3.Dot(playerCam.transform.forward, new Vector3(windDir.x, 0, windDir.y));
+        Debug.Log("the dot product is  " + dotP);
+
+        if (dotP < 0)
+        {
+            playerCam.GetComponent<PostProcessingCamera>().radius = Mathf.Lerp(1.0f, 0.3f, frostIntensity);
+
+            playerCam.GetComponent<PostProcessingCamera>().feather = Mathf.Lerp(1.5f, 0.3f, frostIntensity);
+
+            playerCam.GetComponent<PostProcessingCamera>().Intensity = Mathf.Lerp(0.5f, 1.3f, frostIntensity);
+
+            if (frostIntensity < 1)
+            {
+                frostIntensity += (Time.deltaTime * (-1 * dotP)) / accumulationRate;
+            }
+
+            if (!firstMelt && frostIntensity > 0.4f) // make sure droptlets dont render unless a minimum amount of frost accumulated on the screen
+            {
+                firstMelt = true;
+            }
+        }
+        else
+        {
+            if (firstMelt) // adjust shader pass according to the amount of frost on the screen when melting starts after accumulating 
+            {
+                firstMelt = false;
+
+                distort = frostIntensity;
+
+                if (frostIntensity > 0.5f && frostIntensity < 0.6f)
+                {
+                    playerCam.GetComponent<PostProcessingCamera>().waterShaderPass = 0; // the higher the shader pass the more water droplets on the screen
+                    playerCam.GetComponent<PostProcessingCamera>().timeScale = 2.0f;
+                }
+
+                else if (frostIntensity > 0.6f && frostIntensity < 0.8f)
+                {
+                    playerCam.GetComponent<PostProcessingCamera>().waterShaderPass = 1;
+                    playerCam.GetComponent<PostProcessingCamera>().timeScale = 3.0f;
+                }
+
+                else if (frostIntensity > 0.8f)
+                {
+                    playerCam.GetComponent<PostProcessingCamera>().waterShaderPass = 2;
+                    playerCam.GetComponent<PostProcessingCamera>().timeScale = 4.0f;
+                }
+            }
+
+            yield return new WaitForSeconds(timeBeforeMelt); //wait 2 seconds before snow accumulation melts
+
+            playerCam.GetComponent<PostProcessingCamera>().radius = Mathf.Lerp(1.0f, 0.3f, frostIntensity);
+
+            playerCam.GetComponent<PostProcessingCamera>().feather = Mathf.Lerp(1.5f, 0.3f, frostIntensity);
+
+            playerCam.GetComponent<PostProcessingCamera>().Intensity = Mathf.Lerp(0.5f, 1.3f, frostIntensity);
+
+            if (frostIntensity >= 0)
+            {
+                frostIntensity -= Time.deltaTime / meltingRate;
+            }
+        }
+
+        if (distort > 0)
+        {
+            distort -= Time.deltaTime / meltingRate;
+        }
+
+        playerCam.GetComponent<PostProcessingCamera>().distortion = Mathf.Lerp(0, 5.0f, distort);
     }
 }
--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -9,12 +9,13 @@ using UnityEngine;
 public class Wind : MonoBehaviour
 {
     //Weather
-    public enum Weather {Blizzard, Snowy, Clear};
+    public enum Weather { Blizzard, Snowy, Clear };
     public Weather currentWeather = Weather.Clear;
-    
+
     //Wind
     public Vector2 oldWindDir, currentWindDir, newWindDir;
-    [SerializeField] private float windIntesity = 2, windDirectionTimeSlotMin, windDirectionTimeSlotMax;
+
+    [SerializeField] private float windIntesity = 200, windDirectionTimeSlotMin, windDirectionTimeSlotMax;
     private float windDirectionTimeSlot, windDirectionCountDown = 0;
 
     //Particle System
@@ -22,6 +23,7 @@ public class Wind : MonoBehaviour
 
     private ParticleSystem.VelocityOverLifetimeModule particleVelOverLifeTime1, particleVelOverLifeTime2, particleVelOverLifeTime3, particleVelOverLifeTime4, particleVelOverLifeTime5;
     private ParticleSystem.MinMaxCurve minMaxX, minMaxY, minMaxZ;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -39,38 +41,36 @@ public class Wind : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if (windDirectionCountDown <= 0)
-        // {
-        //     windDirectionTimeSlot = Random.Range(windDirectionTimeSlotMin, windDirectionTimeSlotMax);
-        //     windDirectionCountDown = windDirectionTimeSlot;
+        if (windDirectionCountDown <= 0)
+        {
+            windDirectionTimeSlot = Random.Range(windDirectionTimeSlotMin, windDirectionTimeSlotMax);
+            windDirectionCountDown = windDirectionTimeSlot;
 
-        //     newWindDir = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
-        //     oldWindDir = currentWindDir;
-        // }
+            newWindDir = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+            oldWindDir = currentWindDir;
+        }
 
-        // currentWindDir = Vector2.Lerp(oldWindDir, newWindDir, windDirectionCountDown/windDirectionTimeSlot);
+        currentWindDir = Vector2.Lerp(oldWindDir, newWindDir, windDirectionCountDown/windDirectionTimeSlot);
 
-        // minMaxX = new ParticleSystem.MinMaxCurve(currentWindDir.x, currentWindDir.x * windIntesity);
-        // minMaxZ = new ParticleSystem.MinMaxCurve(currentWindDir.y, currentWindDir.y * windIntesity); //wind direction is 2d so y is z in this case
+        minMaxX = new ParticleSystem.MinMaxCurve(currentWindDir.x, currentWindDir.x * windIntesity);
+        minMaxZ = new ParticleSystem.MinMaxCurve(currentWindDir.y, currentWindDir.y * windIntesity); //wind direction is 2d so y is z in this case
 
-        // particleVelOverLifeTime1.x = minMaxX;
-        // particleVelOverLifeTime1.z = minMaxZ;
+        particleVelOverLifeTime1.x = minMaxX;
+        particleVelOverLifeTime1.z = minMaxZ;
 
-        // particleVelOverLifeTime2.y = new ParticleSystem.MinMaxCurve(-currentWindDir.x, -currentWindDir.x * windIntesity);
-        // particleVelOverLifeTime2.z = minMaxZ;
+        particleVelOverLifeTime2.y = new ParticleSystem.MinMaxCurve(-currentWindDir.x, -currentWindDir.x * windIntesity);
+        particleVelOverLifeTime2.z = minMaxZ;
 
-        // particleVelOverLifeTime3.y = new ParticleSystem.MinMaxCurve(-currentWindDir.x, -currentWindDir.x * windIntesity);
-        // particleVelOverLifeTime3.z = minMaxZ;
+        particleVelOverLifeTime3.y = new ParticleSystem.MinMaxCurve(-currentWindDir.x, -currentWindDir.x * windIntesity);
+        particleVelOverLifeTime3.z = minMaxZ;
 
-        // particleVelOverLifeTime4.z = minMaxX;
-        // particleVelOverLifeTime4.y = minMaxZ;
+        particleVelOverLifeTime4.z = minMaxX;
+        particleVelOverLifeTime4.y = minMaxZ;
 
-        // particleVelOverLifeTime5.z = minMaxX;
-        // particleVelOverLifeTime5.y = minMaxZ;
+        particleVelOverLifeTime5.z = minMaxX;
+        particleVelOverLifeTime5.y = minMaxZ;
 
-        // windDirectionCountDown -= Time.deltaTime;
-
-        currentWindDir = new Vector2(1,0);
+        windDirectionCountDown -= Time.deltaTime;
 
         //Debug.Log("The current wind direction is " + currentWindDir + " The new wind direction is " + newWindDir + " the wind direction countdown is " + windDirectionCountDown);
     }
